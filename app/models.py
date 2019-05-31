ef162c6ce2312b229a8bbb3bd3466b49dca846a4
Christine <you@example.com>
2019-05-31 11:07:23 +0300

    modifies quote class model

--- app/models.py
+++ app/models.py
@@ -106,7 +106,7 @@ class Comment(db.Model):
 
 
 class Quote:
-    def __init__(self,id, author, quote)
+    def __init__(self,id,quote,author):
         self.id = id
         self.quote = quote
         self.author = author
