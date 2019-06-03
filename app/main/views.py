986abb408457422d65314bf961809bdb499c7d3b
Christine <you@example.com>
2019-06-03 12:14:29 +0300

    adds delete functionality on blog and comment

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,77 @@
 from flask import render_template, request, redirect, url_for,abort
+from ..requests import get_quote
+from flask_login import login_required
 from . import main
-# from ..request import get_quote
+from ..models import Comment, User, Post
+from .forms import CommentForm, UpdateProfile, PostForm
+from flask_login import login_required,current_user
+from ..import db,photos
 
 
+@main.route("/",methods=["GET","POST"])
+def index():
+    random_quote = get_quote()
+    categories=["FASHION & BEAUTY", "ART", "CAREER & FINANCE", "MOTHERHOOD", "GAMING", "MUSIC"]
+    return render_template('index.html',categories = categories,quotes=random_quote)
 
 
+@main.route("/blog/<string:category>")
+def posts(category):
+    posts=list(Post.query.filter_by(category=category))
+    return render_template("/blog.html",posts=posts)
 
 
-@main.route('/blog/<int:id>')
-def blog(id):
+@main.route("/post",methods=["GET","POST"])
+@login_required
+def add():
+    post_form=PostForm()
+    if post_form.validate_on_submit():
+        post=Post(title=post_form.title.data, text=post_form.post.data,category=post_form.category.data, user_id=current_user.id, username=current_user.username)
+        
+        post.save_post()
+        return redirect(url_for("main.index"))
+    return render_template('./post.html',post_form=post_form)
 
-    quote = get_quote(id)
-    author = f'{blog.author}'
-    return render_template('blog.html', author=author, quote = quote)
+
+@main.route("/delete/<int:id>",methods=["GET","POST"])
+@login_required
+def delete(id):
+    deleted = Post.query.filter_by(id=id).first()
+    db.session.delete(deleted)
+    db.session.commit()
+    return redirect (url_for('main.index'))
+
+
+@main.route("/commentDelete/<int:id>",methods=["GET","POST"])
+@login_required
+def delete_comment(id):
+    deleted_comment = Comment.query.filter_by(id=id).first()
+    db.session.delete(deleted_comment)
+    db.session.commit()
+    return redirect (url_for('main.index'))
+  
+
+
+@main.route('/comment/new/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(post_id):
+    form = CommentForm()
+    post = Post.query.filter_by(id=post_id).first()
+
+    if form.validate_on_submit():
+        comment = form.comment.data
+
+        # Updated comment instance
+        new_comment = Comment(text=comment,user_id=current_user.id, post_id=post_id)
+
+        # save review method
+        db.session.add(new_comment)
+        db.session.commit()
+        
+       
+    all_comments = Comment.query.filter_by(post_id=post_id).all()
+    
+    return render_template('comment.html',form=form, comments=all_comments, post=post)
 
 
 
@@ -56,4 +115,11 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile', uname=uname))
+
+
+
+
+
+
+    
 
